23137361ed947edb46ec5668ff849755f79064ca
sujay <SujayW@ndtv.com>
2021-10-03 00:51:48 +0530

    added delete product property/function to cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,71 +2,18 @@ import React from "react";
 import { render } from "react-dom";
 
 class CartItem extends React.Component {
- 
 
-    // testing() {
-    //     const promise = new promise ((resolve, reject) => {
-    //       setTimeout(() => {
-    //          resolve('done');
 
-    //       }, 5000); 
-    //     })
-
-    //     promise.then(() => {
-    //         //setState acts like a synchronous call 
-    //         this.setState({qty: 100});
-
-    //         console.log('state', this.state);
-    //     });
-
-    // }
-
-    increaseQuantity = () => {
-        // this.state.qty += 1;
-        // console.log("this.state" , this.state);
-        //-----------------setState form 1------------------
-        // this.setState ({
-        //     qty: this.state.qty + 1
-        // });
-        //-----------------setState form 2------------------
-        this.setState((prevState) => {
-            return{
-                qty: prevState.qty + 1
-            }
-        },() => {
-            console.log("this.state",this.state);
-        });
-
-        // console.log("this.state" ,this.state); ----because our setState call is asynchronous 
-    }
-
-    decreaseQuantity = () => {
-        // this.state.qty += 1;
-        // console.log("this.state" , this.state);
-        //-----------------setState form 1------------------
-        // this.setState ({
-        //     qty: this.state.qty + 1
-        // });
-        //-----------------setState form 2------------------
-        const { qty } = this.state // using Object distructuring
-        if (qty === 0){
-            return;
-        }
-        this.setState((prevState) => {
-            return{
-                qty: prevState.qty - 1
-            }
-        },() => {
-                console.log("this.state",this.state)
-        });
-    }
-
-   
-    
 render(){
 
     console.log('this.props', this.props);
     const {price, title, qty, img} = this.props.product; // using Object distructuring
+    const {
+        product, 
+        onIncreaseQuantity, 
+        onDecreaseQuantity, 
+        onDeleteProduct
+    } = this.props; // using Object distructuring
     return(
         <div className="cart-item">
             
@@ -83,16 +30,17 @@ render(){
                     <img alt="increase" 
                     className="action-icons" 
                     src="https://cdn-icons-png.flaticon.com/512/1828/1828926.png"
-                    onClick = {this.increaseQuantity}
+                    onClick = {() => onIncreaseQuantity(product)}
                     />
                     <img alt="decrease" 
                     className="action-icons" 
                     src="https://cdn-icons-png.flaticon.com/512/1828/1828906.png"
-                    onClick = {this.decreaseQuantity}
+                    onClick = {() => onDecreaseQuantity(product)}
                     />
                     <img alt="delete" 
                     className="action-icons" 
                     src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+                    onClick = {() => onDeleteProduct(product.id)}
                     />
                 </div>
             </div>
