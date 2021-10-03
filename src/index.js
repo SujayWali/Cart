912dfde78306c35f68841f579284ba64235fae62
sujay <SujayW@ndtv.com>
2021-10-04 01:06:15 +0530

    just installed firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLpmCYwlArZNdBJ-6bCHDK-yue7ODCuUw",
+  authDomain: "cart-604d0.firebaseapp.com",
+  projectId: "cart-604d0",
+  storageBucket: "cart-604d0.appspot.com",
+  messagingSenderId: "832564564744",
+  appId: "1:832564564744:web:0ca5990623ba29fabc92bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
 ReactDOM.render(
